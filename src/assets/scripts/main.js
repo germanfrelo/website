985d590ffad02bd429016508d7accbfdb85373bb
Germán Freixinós López <14282156+germanfrelo@users.noreply.github.com>
2021-11-28 21:37:24 +0100

    Complete redesign with CUBE CSS, update about content

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,29 +1,37 @@
+const root = document.documentElement;
 document.documentElement.classList.remove("no-js");
 
 
-// ---------- Page theme toggle ----------
+// ---------- Theme Toggle ----------
 
-const themeToggleForm = document.querySelector("#theme-toggle-form");
-const currentTheme = document.documentElement.dataset.theme;
+const themeToggle = document.querySelector("#theme-toggle-form");
+const currentTheme = root.dataset.theme;
+
 const savedTheme = localStorage.getItem("theme");
 
 if (savedTheme && (savedTheme !== currentTheme)) {
-	themeToggleForm.querySelector(`input[value='${savedTheme}']`).checked = true;
-	document.documentElement.dataset.theme = savedTheme;
+	themeToggle.querySelector(`input[value='${savedTheme}']`).checked = true;
+	root.dataset.theme = savedTheme;
 }
 
-themeToggleForm.addEventListener("change", (e) => {
+themeToggle.addEventListener("change", (e) => {
 	const selectedInput = e.target.closest("input[type='radio']");
+
 	if (selectedInput) {
-		document.documentElement.dataset.theme = selectedInput.value;
-		localStorage.setItem("theme", selectedInput.value);
+		const selectedTheme = selectedInput.value;
+
+		root.dataset.theme = selectedTheme;
+		localStorage.setItem("theme", selectedTheme);
+
+		let colorScheme;
+
+		if (selectedTheme !== "system") {
+			colorScheme = selectedTheme;
+		} else {
+			colorScheme = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark light" : "light dark";
+		}
+
+		document.querySelector("meta[name='color-scheme']").content = colorScheme;
+		root.style.colorScheme = colorScheme;
 	}
-});
-
-
-// ---------- Nav/Menu toggle ----------
-const navToggle = document.querySelector(".nav-toggle");
-navToggle && navToggle.addEventListener("click", function() {
-	let t = "true" === navToggle.getAttribute("aria-expanded");
-	navToggle.setAttribute("aria-expanded", !t)
 });
