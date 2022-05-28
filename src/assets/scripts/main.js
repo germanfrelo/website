fdb64403ef8ed2f3b5bb3dff7f675bcaeda2b135
Germán Freixinós López <14282156+germanfrelo@users.noreply.github.com>
2022-05-29 00:00:14 +0200

    Add Prettier and format the code

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,94 +1,101 @@
-document.documentElement.classList.remove("no-js")
+document.documentElement.classList.remove("no-js");
 
 // ---------- Theme Toggle ----------
 
-const savedTheme = localStorage.getItem("theme")
-const currentTheme = document.documentElement.dataset.theme
-const themeToggle = document.querySelector("#theme-toggle")
+const savedTheme = localStorage.getItem("theme");
+const currentTheme = document.documentElement.dataset.theme;
+const themeToggle = document.querySelector("#theme-toggle");
 
-if (savedTheme && (savedTheme !== currentTheme)) {
-	themeToggle.querySelector(`input[value='${savedTheme}']`).checked = true
-	setRootDataTheme(savedTheme)
-	setColorScheme(savedTheme)
-	setImagesByTheme(savedTheme)
+if (savedTheme && savedTheme !== currentTheme) {
+	themeToggle.querySelector(`input[value='${savedTheme}']`).checked = true;
+	setRootDataTheme(savedTheme);
+	setColorScheme(savedTheme);
+	setImagesByTheme(savedTheme);
 }
 
 themeToggle.addEventListener("change", (e) => {
-	const selectedInput = e.target.closest("input[type='radio']")
+	const selectedInput = e.target.closest("input[type='radio']");
 
 	if (selectedInput) {
-		const selectedTheme = selectedInput.value
+		const selectedTheme = selectedInput.value;
 
-		setRootDataTheme(selectedTheme)
-		setColorScheme(selectedTheme)
-		setImagesByTheme(selectedTheme)
-		updateSavedTheme(selectedTheme)
+		setRootDataTheme(selectedTheme);
+		setColorScheme(selectedTheme);
+		setImagesByTheme(selectedTheme);
+		updateSavedTheme(selectedTheme);
 	}
-})
+});
 
 function updateSavedTheme(theme) {
-	localStorage.setItem("theme", theme)
+	localStorage.setItem("theme", theme);
 }
 
 function setRootDataTheme(theme) {
-	document.documentElement.dataset.theme = theme
+	document.documentElement.dataset.theme = theme;
 }
 
 function setColorScheme(theme) {
-	let colorScheme = "light dark"
+	let colorScheme = "light dark";
 
 	switch (theme) {
 		case "light":
 		case "dark":
-			colorScheme = theme
-			break
+			colorScheme = theme;
+			break;
 
 		case "system":
-			colorScheme = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark light" : "light dark"
-			break
+			colorScheme = window.matchMedia("(prefers-color-scheme: dark)").matches
+				? "dark light"
+				: "light dark";
+			break;
 
 		default:
-			break
+			break;
 	}
 
 	// In the meta tag in HTML
-	document.querySelector("meta[name='color-scheme']").content = colorScheme
+	document.querySelector("meta[name='color-scheme']").content = colorScheme;
 	// In CSS
-	document.documentElement.style.setProperty("--color-scheme", colorScheme)
+	document.documentElement.style.setProperty("--color-scheme", colorScheme);
 }
 
 function setImagesByTheme(selectedTheme) {
-	const imagesAndSources = document.querySelectorAll("[srcset], [src]")
+	const imagesAndSources = document.querySelectorAll("[srcset], [src]");
 
 	switch (selectedTheme) {
 		case "light":
-			imagesAndSources.forEach(element => {
-				if (element.matches("img")) element.src = element.src.replace("[dark]", "[light]")
-				if (element.matches("source")) element.srcset = element.srcset.replace("[dark]", "[light]")
-			})
-			break
+			imagesAndSources.forEach((element) => {
+				if (element.matches("img"))
+					element.src = element.src.replace("[dark]", "[light]");
+				if (element.matches("source"))
+					element.srcset = element.srcset.replace("[dark]", "[light]");
+			});
+			break;
 
 		case "dark":
-			imagesAndSources.forEach(element => {
-				if (element.matches("img")) element.src = element.src.replace("[light]", "[dark]")
-				if (element.matches("source")) element.srcset = element.srcset.replace("[light]", "[dark]")
-			})
-			break
+			imagesAndSources.forEach((element) => {
+				if (element.matches("img"))
+					element.src = element.src.replace("[light]", "[dark]");
+				if (element.matches("source"))
+					element.srcset = element.srcset.replace("[light]", "[dark]");
+			});
+			break;
 
 		case "system":
-			imagesAndSources.forEach(element => {
-				if (element.matches("img")) element.src = element.src.replace("[dark]", "[light]")
+			imagesAndSources.forEach((element) => {
+				if (element.matches("img"))
+					element.src = element.src.replace("[dark]", "[light]");
 				if (element.matches("source")) {
 					if (element.matches("[media*='light']")) {
-						element.srcset = element.srcset.replace("[dark]", "[light]")
+						element.srcset = element.srcset.replace("[dark]", "[light]");
 					} else if (element.matches("[media*='dark']")) {
-						element.srcset = element.srcset.replace("[light]", "[dark]")
+						element.srcset = element.srcset.replace("[light]", "[dark]");
 					}
 				}
-			})
-			break
+			});
+			break;
 
 		default:
-			break
+			break;
 	}
 }
