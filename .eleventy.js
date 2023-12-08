df68124dc2888cf673333e5b344fdab7f25c7819
Germán Freixinós López <14282156+germanfrelo@users.noreply.github.com>
2023-12-08 14:11:57 +0100

    Remove unnecessary 11ty config code

--- .eleventy.js
+++ .eleventy.js
@@ -4,8 +4,6 @@ module.exports = function (eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("_redirects");
 
 	return {
-		markdownTemplateEngine: "njk",
-		dataTemplateEngine: "njk",
 		htmlTemplateEngine: "njk",
 		dir: {
 			input: "src",
