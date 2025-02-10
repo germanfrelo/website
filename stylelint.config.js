d93b1c6a10dd8ee4b3efa413ee6d1eaa614cfefe
Germán Freixinós <14282156+germanfrelo@users.noreply.github.com>
2025-02-10 21:45:38 +0100

    chore: improve Stylelint configuration (#234)
    * Improve Stylelint configuration
    
    Sync from https://github.com/germanfrelo/template/blob/0a8c68f4755e6a276d16e2ffab2a2cea99d3d9c6/stylelint.config.js
    
    * Fix Stylelint errors

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,25 +1,58 @@
 /** @type {import('stylelint').Config} */
 export default {
 	extends: [
-		// Order matters: later configs take precedence over (override) earlier ones
-		"stylelint-config-standard",
-		"stylelint-config-recess-order",
+		// The order of configs is important: later configs take precedence over earlier ones
+		"stylelint-config-standard", // Extends stylelint-config-recommended and turns on additional rules to enforce modern conventions
+		"stylelint-config-recess-order", // Sorts CSS properties
 	],
+	plugins: ["stylelint-plugin-defensive-css"], // Enforces defensive CSS best practices
 	reportDescriptionlessDisables: true,
 	reportInvalidScopeDisables: true,
 	reportNeedlessDisables: true,
+	reportUnscopedDisables: true,
 	rules: {
-		// Avoid errors: Unknown
-		"declaration-property-value-no-unknown": true,
-		"media-feature-name-value-no-unknown": true,
+		/* Avoid errors
+		---------------------------------------- */
+		// Descending
+		"no-descending-specificity": [
+			true, // Already enabled in stylelint-config-recommended
+			{ severity: "warning" }, // Default is 'error', but I prefer 'warning'
+		],
+		// Unknown
 		"no-unknown-animations": true,
 		"no-unknown-custom-media": true,
 		"no-unknown-custom-properties": true,
-		// Enforce conventions: Patterns
+
+		/* Enforce conventions
+		(overrides rules from stylelint-config-standard)
+		---------------------------------------- */
+		// Empty lines
+		"declaration-empty-line-before": "never",
+		// Notation
+		"media-feature-range-notation": null, // TODO: Remove when 'baseline' is 'widely available' (~96%) (https://caniuse.com/css-media-range-syntax)
+		// Pattern
 		"custom-media-pattern": null,
 		"custom-property-pattern": null,
 		"keyframes-name-pattern": null,
 		"selector-class-pattern": null,
 		"selector-id-pattern": null,
+		// Redundant
+		"declaration-block-no-redundant-longhand-properties": null,
+
+		/* Plugin: use-defensive-css
+		---------------------------------------- */
+		"plugin/use-defensive-css": [
+			true,
+			{
+				"severity": "warning",
+				"accidental-hover": false, // Enable as needed
+				"background-repeat": false, // The imported CSS reset already apply 'no-repeat' to all elements
+				"custom-property-fallbacks": true,
+				"flex-wrapping": true,
+				"scroll-chaining": true,
+				"scrollbar-gutter": false, // Enable as needed
+				"vendor-prefix-grouping": true,
+			},
+		],
 	},
 };
