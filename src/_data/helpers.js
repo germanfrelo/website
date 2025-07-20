0b25990a067d8a5a2d79ae4aa474ed3d292a7687
Germán Freixinós <14282156+germanfrelo@users.noreply.github.com>
2025-07-20 19:10:01 +0200

    feat: Minor website redesign (#282)
    * WIP
    
    * Change content and redesign status badge
    
    * Don't center page content vertically
    
    * Disable CSS lint rule "custom-property-fallbacks"
    
    * Remove unused colors variables
    
    * refactor: Use longhand transition properties
    
    * Change link underline styles
    
    * Remove duplicated/unused CSS rule
    
    * Add arrow icon to external links
    
    * Add transition to external link icon color

--- src/_data/helpers.js
+++ src/_data/helpers.js
@@ -11,11 +11,11 @@ export default {
 		let response = "";
 
 		if (itemUrl === pageUrl) {
-			response = " aria-current='page'";
+			response = ' aria-current="page"';
 		}
 
 		if (itemUrl.length > 1 && pageUrl.indexOf(itemUrl) === 0) {
-			response += " data-state='active'";
+			response += ' data-state="active"';
 		}
 
 		return response;
